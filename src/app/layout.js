b448205081744c3f0dc290ac45650c8635d898a9
donghun kim <gnsia@naver.com>
2024-04-15 22:37:56 +0900

    tailwind 넣어보기...

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Control } from "./Control";
 import "./globals.css";
 import Link from "next/link";
 import { myServerClient } from "../../utils/supabase/server";
+import { Control } from "./Control";
+import { Nav } from "./Nav";
 
 export const revalidate = 0;
 
@@ -17,7 +18,25 @@ export default async function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <h1><Link href="/">공감</Link></h1>
+        <Nav />
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+        <header>
+          <h1><Link href="/">공감</Link></h1>
+          <nav>
+            <ul>
+              <li><Link href="/reservation">예약</Link></li>
+              <li><Link href="/therapist">치료사</Link></li>
+              <li><Link href="/patients">환자</Link></li>
+            </ul>
+          </nav>
+        </header>
         <ol>
           {page.map(p => 
             <li key={p.id}>
@@ -25,7 +44,7 @@ export default async function RootLayout({ children }) {
             </li>
           )}
         </ol>
-          {children}
+          
           <Control />
       </body>
     </html>
